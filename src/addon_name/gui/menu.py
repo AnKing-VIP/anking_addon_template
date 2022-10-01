923c2c3546c096184c78467c9330dea3fcbf948b
RisingOrange <jakub.fidler@protonmail.com>
2022-10-01 19:54:06 +0200

    Satisfy mypy

--- src/addon_name/gui/menu.py
+++ src/addon_name/gui/menu.py
@@ -1,4 +1,4 @@
-from aqt.qt import QAction
+from aqt.qt import QAction, qconnect
 
 from .anking_menu import get_anking_menu
 
@@ -7,4 +7,4 @@ def setup_menu() -> None:
     menu = get_anking_menu()
     a = QAction("Menu Name", menu)
     menu.addAction(a)
-    a.triggered.connect(lambda _: print("abc"))
+    qconnect(a.triggered, (lambda _: print("abc")))
